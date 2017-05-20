494c37b3287e122c1053d8c67ba8bfe88721503a
Min <m1nk1m@Minseoks-MacBook-Pro.local>
2017-05-19 23:43:30 -0400

    state
    Introduction to State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 // const Component = React.Component;
 
 class SearchBar extends Component {
-  render() {
-    return <input onChange={this.onInputChange} />;
-    // on+eventName={this.eventHandler}
+
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
   }
 
-  onInputChange(event) {
-    console.log(event.target.value);
-  } // generally, handleInputChange or onInputChange
+  render() {
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value  })} />
+        Value of the input: {this.state.term}
+      </div>
+    );
+    // on+eventName={this.eventHandler}
+
+  }
+
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // } // generally, handleInputChange or onInputChange
 }
 
 // const SearchBar = () => {
